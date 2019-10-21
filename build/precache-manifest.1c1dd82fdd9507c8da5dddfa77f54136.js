0607d8d050db2a3318ccc156f778d61d45ccadac
deanvlue <munoz.josecarlos@gmail.com>
2019-10-20 21:57:40 -0500

    Push build

---
+++ build/precache-manifest.1c1dd82fdd9507c8da5dddfa77f54136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fe4bf8a7bab7a9249ad02819ee4bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f639a58344768393c5d",
+    "url": "/static/css/main.7ff2ac69.chunk.css"
+  },
+  {
+    "revision": "4d43b1a5439c51312aed",
+    "url": "/static/js/2.248cf5d1.chunk.js"
+  },
+  {
+    "revision": "6f639a58344768393c5d",
+    "url": "/static/js/main.71b116c4.chunk.js"
+  },
+  {
+    "revision": "6a54604de3c4de61136a",
+    "url": "/static/js/runtime-main.9de3d65c.js"
+  },
+  {
+    "revision": "19b5ef5e482295da7d85e8b13b0f9069",
+    "url": "/static/media/btcicon.19b5ef5e.png"
+  }
+]);
